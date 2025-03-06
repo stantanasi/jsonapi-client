c133db4357813ebd2d4d52ff24fbb3c75e5e6243
Lory-Stan TANASI <stantanasi@gmail.com>
2025-03-06 21:31:48 +0100

    feat: export files from package

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export {
+  connect
+} from './lib/client'
+
+export {
+  resource,
+  Resource,
+  TResource
+} from './lib/resource'
+
+export {
+  default as Query
+} from './lib/query'
+
+export {
+  default as Schema
+} from './lib/schema'
+
