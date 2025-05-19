135ccc7f40d59c422164ceddeddd952fa60ceef9
Lory-Stan TANASI <stantanasi@gmail.com>
2025-05-19 08:55:08 +0200

    feat(example): add example of fetching relationships

--- example/src/index.ts
+++ example/src/index.ts
@@ -14,6 +14,14 @@ Article.find()
 Article.findById('1')
   .then((article) => console.log(article));
 
+// GET /articles/1/author HTTP/1.1
+Article.findById('1').get('author')
+  .then((author) => console.log(author));
+
+// GET /articles/1/comments HTTP/1.1
+Article.findById('1').get('comments')
+  .then((comments) => console.log(comments));
+
 
 // GET /comments HTTP/1.1
 Comment.find()
@@ -23,6 +31,14 @@ Comment.find()
 Comment.findById('5')
   .then((comment) => console.log(comment));
 
+// GET /comments/5/article HTTP/1.1
+Comment.findById('5').get('article')
+  .then((article) => console.log(article));
+
+// GET /comments/5/author HTTP/1.1
+Comment.findById('5').get('author')
+  .then((author) => console.log(author));
+
 
 // GET /people HTTP/1.1
 People.find()
@@ -31,3 +47,11 @@ People.find()
 // GET /people/9 HTTP/1.1
 People.findById('9')
   .then((people) => console.log(people));
+
+// GET /people/9/articles HTTP/1.1
+People.findById('9').get('articles')
+  .then((articles) => console.log(articles));
+
+// GET /people/9/comments HTTP/1.1
+People.findById('9').get('comments')
+  .then((comments) => console.log(comments));
